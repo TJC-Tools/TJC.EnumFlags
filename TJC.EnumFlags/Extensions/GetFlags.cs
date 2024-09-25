ebd707cd73b7e18fd195ad2bd8eef228f0f231a1
Tyler Carrol <tylercarrol@hotmail.com>
2024-09-25 19:38:15 -0400

    Create GetFlags Extension

---
+++ TJC.EnumFlags/Extensions/GetFlags.cs
@@ -0,0 +1,25 @@
+﻿namespace TJC.EnumFlags.Extensions;
+
+public static partial class EnumFlags
+{
+    /// <summary>
+    /// Gets all the flags in an enum.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="flags"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static IEnumerable<T> GetFlags<T>(this T flags) where T : struct, Enum
+    {
+        if (!typeof(T).IsEnum)
+            throw new ArgumentException($"{nameof(T)} [{typeof(T).Name}] must be an enum.");
+
+        var flagsValue = Convert.ToUInt64(flags);
+        foreach (T flag in Enum.GetValues(typeof(T)))
+        {
+            var flagValue = Convert.ToUInt64(flag);
+            if (flagValue != 0 && (flagsValue & flagValue) == flagValue)
+                yield return flag;
+        }
+    }
+}
