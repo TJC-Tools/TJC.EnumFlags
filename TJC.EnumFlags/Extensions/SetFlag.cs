d162ca9e325ab27c4cb9704d7d56c44c80fb040e
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-28 22:00:35 -0400

    🧹 chore: [MegaLinter] Apply [1] automatic fixes (#33)
    * ✨ feature: Add MegaLinter Workflow
    
    * Create .jscpd.json to ignore duplication within .Tests
    
    * Update .jscpd.json to include duplication threshold
    
    * 🧹 chore: [MegaLinter] Apply [1] automatic fixes
    
    ---------
    
    Co-authored-by: TylerCarrol <25536704+TylerCarrol@users.noreply.github.com>

--- TJC.EnumFlags/Extensions/SetFlag.cs
+++ TJC.EnumFlags/Extensions/SetFlag.cs
@@ -15,7 +15,8 @@ public static partial class EnumFlags
     /// <param name="flag"></param>
     /// <returns></returns>
     /// <exception cref="ArgumentException"></exception>
-    public static void SetFlag<T>(this ref T flags, T flag) where T : struct, Enum
+    public static void SetFlag<T>(this ref T flags, T flag)
+        where T : struct, Enum
     {
         if (!typeof(T).IsEnum)
             throw new ArgumentException($"{nameof(T)} [{typeof(T).Name}] must be an enum.");
